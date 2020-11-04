0749b239608926c1dcfd96e171b47749d4084e6f
Soksurim <briochefavori@gmail.com>
2020-11-04 10:05:21 +0900

    123
    123

--- src/mario/PlayGame.java
+++ src/mario/PlayGame.java
@@ -13,3 +13,31 @@ public class PlayGame {
 		new MarioLogin();
 	}
 }
+
+/*
+
+
+create table mario(
+seq number primary key,
+clientaccount varchar2(30),
+password varchar2(15),
+realname varchar2(15),
+age number,
+nickname varchar2(30),
+gender number,
+infoagree number,
+score number,
+goaltime varchar2(24),
+playerrank number);
+
+create sequence seq_mario nocycle nocache;
+
+select seq_mario.nextval from dual;
+
+select * from mario;
+
+drop table mario;
+
+drop sequence seq_mario;
+
+*/
